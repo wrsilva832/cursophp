024a3b0de05cdf7de4d905aa0d7cb27e541f6f43
wrsilva832 <wrsilva832@gmail.com>
2023-03-29 22:03:23 -0300

    atualização

--- exemplos-php/interacao-form-php/bem-vindo-get.php
+++ exemplos-php/interacao-form-php/bem-vindo-get.php
@@ -2,10 +2,12 @@
 // exemplo usando o metodo GET
 $nome = $_GET["nome"];
 $email = $_GET["email"];
+$Idade = $_GET["Idade"];
 
 
 echo "Bem Vindo ao mundo PHP $nome <br>";
 echo "E-mail registrado $email";
+echo "Idade registrada $Idade";
 
 
 
--- exemplos-php/interacao-form-php/bem-vindo-post.php
+++ exemplos-php/interacao-form-php/bem-vindo-post.php
@@ -2,10 +2,12 @@
 // exemplo usando o metodo POST
 $nome = $_POST["nome"];
 $email = $_POST["email"];
+$Idade = $_POST["Idade"];
 
 
 echo "Bem Vindo ao mundo PHP $nome <br>";
 echo "E-mail registrado $email";
+echo "Idade registrado $Idade";
 
 
 
