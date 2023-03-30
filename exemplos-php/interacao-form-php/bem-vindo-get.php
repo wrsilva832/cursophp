bf4987bf1a215d6fe8af9524167cc0a7d1edbf6b
wrsilva832 <wrsilva832@gmail.com>
2023-03-29 21:36:19 -0300

    first commit

---
+++ exemplos-php/interacao-form-php/bem-vindo-get.php
@@ -0,0 +1,13 @@
+<?php
+// exemplo usando o metodo GET
+$nome = $_GET["nome"];
+$email = $_GET["email"];
+
+
+echo "Bem Vindo ao mundo PHP $nome <br>";
+echo "E-mail registrado $email";
+
+
+
+
+?>
---
+++ exemplos-php/interacao-form-php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <title>Titulo na Aba</title>
+</head>
+<body>
+    <style>
+        /*tag style é um aod lugares para codificar css*/
+        h1{
+           color: rebecc#ffff; 
+           background-color: black;
+           text-align: center;
+        }
+
+    </style>
+   
+ <h1 onclick="alert('')">Minha página</h1>   
+ <script>
+    document.write("Olá mundo JS 2")
+ </script>
+ <?php
+ <a href="exemplos-php/interacao-form-php/index.html">Exemplo Form PHP</a>
+ ?>
+</body>
+</html>
