22926f6b306af5887f852a310e22f984d36aa28f
wrsilva832 <wrsilva832@gmail.com>
2023-04-09 22:53:32 -0300

    Aprimoramento form pessoa

---
+++ exemplos-php/crud-mysql/conectar.php
@@ -0,0 +1,65 @@
+<?php
+// função para fazer a conexão com o banco de dados/
+// essa função retorna uma variavel que tem acesso ao BD.
+function conectar(){
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+
+    $con = new mysqli($servidor, $usuario, $senha, "mydb");
+
+    if($con->connect_error){
+        die("Erro :".$con->connect_error);
+    }
+    //echo "Ok ao conectar";
+    return $con;
+}
+
+// função para incluir uma nova pessoa na tabela
+function incluir($nome, $email, $cpf){
+    $con = conectar();
+    $sql = "insert into pessoa(nome, email, cpf) values('$nome','$email','$cpf')";
+    if($con->query($sql) === true){
+        return "Ok ao gravar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+// buscar todas as pessoas que estão gravadas no banco
+function listar(){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa";
+    $resultado = $con->query($sql);
+    return $resultado;
+}
+
+function buscar($id){
+    $con = conectar();
+    $sql = "select id, nome, email, cpf from pessoa where id = $id";
+    $resultado = $con->query($sql);
+    $resultado = $resultado->fetch_assoc();
+    return $resultado;
+}
+
+function alterar($id, $nome, $email){
+    $con = conectar();
+    $sql = "update pessoa set nome = '$nome', email = '$email', cpf = '$cpf' where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Atualizar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+function apagar($id){
+    $con = conectar();
+    $sql = "delete from pessoa where id = $id";
+    if($con->query($sql) === true){
+        return "Ok ao Apagar";
+    }else{
+        return "Erro: $sql".$con->error;
+    }
+}
+
+?>
---
+++ exemplos-php/crud-mysql/form-pessoa.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Pessoa</title>
+</head>
+<body>
+<?php
+include 'conectar.php';
+$id = $nome = $email = $cp "";
+if($_SERVER["REQUEST_METHOD"] == "GET"f){
+    if (array_key_exists('id',$_GET)){
+        $id = $_GET['id'];
+        $pessoa = buscar($id);
+        $nome = $pessoa['nome'];
+        $email = $pessoa['email'];
+    }
+    if (array_key_exists('apagar',$_GET)){
+        $apagar = $_GET['apagar'];
+        $msg = apagar($apagar);
+        echo $msg;
+    }
+}
+?>
+<form action="form-pessoa.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <h1>Formulário de Pessoa</h1>
+            <label for="">Nome:</label>
+            <input type="text" name="nome" value="<?php echo $nome; ?>"><br>
+            <label for="">E-mail:</label>
+            <input type="text" name="email" value="<?php echo $email; ?>"><br>
+            <label for="">CPF:</label>
+            <input type="text" name="cpf" value="<?php echo $cpf; ?>">
+            <input type="submit" value="Gravar" class="btn_submit">
+</form>
+<?php
+//  onclick="window.location.replace('form-pessoa.php');"
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+
+    $id = $_POST['id'];
+    if($id == ''){
+        $msg = incluir($nome, $email, $cpf);
+    } else {
+        $msg = alterar($id, $nome, $email, $cpf);
+    }
+    
+    //echo $msg;
+}
+
+?>
+<br>
+<table border="1">
+    <tr>
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>cpf</th>
+    </tr>
+    <?php
+    $dados = listar();
+    while ($linha = $dados->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$linha['id']."</td>";
+        echo "<td>".$linha['nome']."</td>";
+        echo "<td>".$linha['email']."</td>";
+        echo "<td>".$linha['cpf']."</td>";
+        echo "<td><a href='form-pessoa.php?id=".$linha['id']."'>Editar</a></td>";
+        echo "<td><a href='form-pessoa.php?apagar=".$linha['id']."'>Apagar</a></td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+</body>
+</html>
