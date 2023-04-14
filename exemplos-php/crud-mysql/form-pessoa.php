cd8a37dff81f43e64e380b526de8e32d5c448d76
wrsilva832 <wrsilva832@gmail.com>
2023-04-14 19:30:08 -0300

    Minha página no 000WebHost.

--- exemplos-php/crud-mysql/form-pessoa.php
+++ exemplos-php/crud-mysql/form-pessoa.php
@@ -47,7 +47,8 @@
                 $senha = $_POST['senha'];
                 $confirmar = $_POST['confirmar'];
                 if ($senha == $confirmar) {
-                    $msg = incluir($nome, $email, $cpf, $sexo, $$escolaridade, $senha);
+                    $msg = incluir($nome, $email, $cpf, $sexo, 
+                    $escolaridade, $senha);
                 } else {
                     $msg = "Senha não confere";
                 }
@@ -74,66 +75,65 @@
         Sexo: <br>
         <input type="radio" name="sexo" value="m" required <?php if ($sexo == "m") echo "checked"; ?>>Masculino<br>
         <input type="radio" name="sexo" value="f" required <?php if ($sexo == "f") echo "checked"; ?>>Feminino<br>
-        <form action="/action_page.php">
-            <label for="cars">Escolaridade:</label>
-            <select id="cars" name="cars">
-                <option value="volvo">Ensino Médio</option>
-                <option value="saab">Superior</option>
-                <option value="fiat">Incompleto</option>
-            </select>
-            <input type="submit">
-        </form>
-        <!-- senha nao pode aparecer quando for editar -->
-        <!-- abre condição -->
-        <!-- se existe a chave id -->
-        <?php if (!array_key_exists('id', $_GET)) { ?>
-            Senha: <br>
-            <input type="password" name="senha"><br>
-            Confirmar:<br>
-            <input type="password" name="confirmar"><br>
-        <?php } ?>
-        <!-- fecha condicao -->
-        <form>
-            <br>
-            <br>
-            <input type="submit" value="Gravar">
-            <a href="form-pessoa.php">
-                <input type="button" value="Novo">
-            </a>
-        </form>
         <br>
-        <table border="1">
-            <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>CPF</th>
-                <th>Sexo</th>
-                <th>Escolaridade</th>
-                <!-- <th>Senha</th> -->
-            </tr>
-            <?php
-            $dados = listar();
-            while ($linha = $dados->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $linha['id'] . "</td>";
-                echo "<td>" . $linha['nome'] . "</td>";
-                echo "<td>" . $linha['email'] . "</td>";
-                echo "<td>" . $linha['cpf'] . "</td>";
-                echo "<td>" . $linha['sexo'] . "</td>";
-                echo "<td>" . $linha['escolaridade'] . "</td>";
-                // echo "<td>" . $senha['senha'] . "</td>"; --comentar pois a senha nao pode aparecer ao editar
-                echo "<td><a href='form-pessoa.php?id=" . $linha['id'] . "'>Editar</a></td>";
-                echo "<td><a onclick='return apagar(" . $linha['id'] . ");' href='form-pessoa.php?apagar=" . $linha['id'] . "'>Apagar</a></td>";
-                echo "</tr>";
+        Escolaridade<br>
+        <select name="escolaridade">
+            <option value="Ensino Médio">Ensino Médio</option>
+            <option value="Superior">Superior</option>
+            <option <?php if($escolaridade == 'Incompleto')>value="Incompleto">Incompleto</option>
+        </select>
+        <br>
+    <!-- senha nao pode aparecer quando for editar -->
+    <!-- abre condição -->
+    <!-- se existe a chave id -->
+    <?php if (!array_key_exists('id', $_GET)) { ?>
+        Senha: <br>
+        <input type="password" name="senha"><br>
+        Confirmar:<br>
+        <input type="password" name="confirmar"><br>
+    <?php } ?>
+    <!-- fecha condicao -->
+    <form>
+        <br>
+        <br>
+        <input type="submit" value="Gravar">
+        <a href="form-pessoa.php">
+            <input type="button" value="Novo">
+        </a>
+    </form>
+    <br>
+    <table border="1">
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>CPF</th>
+            <th>Sexo</th>
+            <th>Escolaridade</th>
+            <!-- <th>Senha</th> -->
+        </tr>
+        <?php
+        $dados = listar();
+        while ($linha = $dados->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $linha['id'] . "</td>";
+            echo "<td>" . $linha['nome'] . "</td>";
+            echo "<td>" . $linha['email'] . "</td>";
+            echo "<td>" . $linha['cpf'] . "</td>";
+            echo "<td>" . $linha['sexo'] . "</td>";
+            echo "<td>" . $linha['escolaridade'] . "</td>";
+            // echo "<td>" . $senha['senha'] . "</td>"; --comentar pois a senha nao pode aparecer ao editar
+            echo "<td><a href='form-pessoa.php?id=" . $linha['id'] . "'>Editar</a></td>";
+            echo "<td><a onclick='return apagar(" . $linha['id'] . ");' href='form-pessoa.php?apagar=" . $linha['id'] . "'>Apagar</a></td>";
+            echo "</tr>";
+        }
+        ?>
+        <script>
+            function apagar(id) {
+                return confirm("Deseja Apagar o registro ID(" + id + ")?");
             }
-            ?>
-            <script>
-                function apagar(id) {
-                    return confirm("Deseja Apagar o registro ID(" + id + ")?");
-                }
-            </script>
-        </table>
+        </script>
+    </table>
 </body>
 
 </html>
---
+++ exemplos-php/exemplo-000webhost/minha-página.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Minha página no 000WebHost</title>
+</head>
+<body>
+<?php
+        echo "Minha página no 000WebHost.";
+        echo "<br>By: Wellington Rodrigues da Silva";
+    ?>
+</body>
+</html>
